3d4220d6910914efbc012993640060c6df22e3b7
Christian Nassau <nassau@nullhomotopie.de>
2003-09-04 15:55:05 +0000

    added matrix_quotient

--- src/adlin.c
+++ src/adlin.c
@@ -14,19 +14,19 @@
 #include "adlin.h"
 
 /* common macros for dealing with the progress variable: */
-#define PROGVARINIT( val )             \
+#define PROGVARINIT                    \
     double perc;                       \
     Tcl_Obj *ProgVar, *NameObj;        \
-    ProgVar = Tcl_NewDoubleObj( val ); \
+    ProgVar = Tcl_NewDoubleObj( 0.0 ); \
     NameObj = Tcl_NewStringObj( LAPROGRESSVAR, sizeof(LAPROGRESSVAR) ); \
     Tcl_IncrRefCount( ProgVar );       \
     Tcl_IncrRefCount( NameObj )      
 
-#define PROGVARSET( val ) 	                                      \
-  Tcl_SetDoubleObj( ProgVar, perc );                                  \
+#define PROGVARSET( val ) if (NULL!=ip)	{                             \
+  Tcl_SetDoubleObj( ProgVar, val );                                   \
   if (NULL==Tcl_ObjSetVar2( ip, NameObj, NULL, ProgVar,               \
 			    TCL_LEAVE_ERR_MSG | TCL_GLOBAL_ONLY ))    \
-       goto done
+       goto done ; }
 
 #define PROGVARDONE \
     Tcl_DecrRefCount( ProgVar ); Tcl_DecrRefCount( NameObj ) 
@@ -44,7 +44,7 @@ matrix *matrix_ortho( primeInfo *pi, matrix *inp, Tcl_Interp *ip, int pmsk) {
     matrix m1, m2;
     matrix *un ;
 
-    PROGVARINIT( 0 );
+    PROGVARINIT ;
 
     un = matrix_create( inp->rows, inp->rows );
     if (NULL == un) return NULL;
@@ -58,10 +58,10 @@ matrix *matrix_ortho( primeInfo *pi, matrix *inp, Tcl_Interp *ip, int pmsk) {
 
     /* make m1, m2 empty copies of inp, resp. un */
 
-    m1.nomcols = inp->nomcols; m1.cols = inp->cols; m1.data = inp->data; m1.rows
- = 0;
-    m2.nomcols = un->nomcols;  m2.cols = un->cols;  m2.data = un->data;  m2.rows
- = 0;
+    m1.nomcols = inp->nomcols; 
+    m1.cols = inp->cols; m1.data = inp->data; m1.rows = 0;
+    m2.nomcols = un->nomcols;  
+    m2.cols = un->cols;  m2.data = un->data;  m2.rows = 0;
 
     for ( v1.data=inp->data, i=0; i<inp->rows; i++, v1.data+=spr ) {
         cint coeff;
@@ -70,7 +70,7 @@ matrix *matrix_ortho( primeInfo *pi, matrix *inp, Tcl_Interp *ip, int pmsk) {
             perc = 1-perc; perc *= perc; perc = 1-perc;
 	    PROGVARSET( perc );
         }
-         /* find pivot for this row */
+	/* find pivot for this row */
         for ( aux=v1.data, j=cols; j; aux++, j-- )
             if ( 0 != *aux ) break;
         if ( 0 == j ) {
@@ -85,7 +85,7 @@ matrix *matrix_ortho( primeInfo *pi, matrix *inp, Tcl_Interp *ip, int pmsk) {
             v3.data = un->data + i * uspr;
             v4.data = v3.data + uspr;
             for ( j=i+1; j<inp->rows; j++, v2.data+=spr, v4.data+=uspr, aux+=spr
- )
+		)
                 if ( 0 != *aux ) {
                     vector_add( &v4, &v3, CINTMULT(*aux,coeff,prime), prime );
                     vector_add( &v2, &v1, CINTMULT(*aux,coeff,prime), prime );
@@ -121,7 +121,7 @@ matrix *matrix_lift( primeInfo *pi, matrix *inp, matrix *lft,
     cint prime = pi->prime;
     matrix *un, *res ;
 
-    PROGVARINIT( 0 );
+    PROGVARINIT ;
 
     un = matrix_create( inp->rows, inp->rows );
     if (NULL == un) return NULL;
@@ -157,18 +157,18 @@ matrix *matrix_lift( primeInfo *pi, matrix *inp, matrix *lft,
             v2.data = v1.data + spr; aux += spr;
             v3.data = un->data + i * uspr;
             v4.data = v3.data + uspr;
-            for ( j=i+1; j<inp->rows; j++, v2.data+=spr, v4.data+=uspr, aux+=spr
- )
+            for ( j=i+1; j<inp->rows; 
+		  j++, v2.data+=spr, v4.data+=uspr, aux+=spr )
                 if ( 0 != *aux ) {
-                    vector_add( &v4, &v3, CINTMULT(*aux,coeff,prime),prime);
-                    vector_add( &v2, &v1, CINTMULT(*aux,coeff,prime),prime);
+                    vector_add( &v4, &v3, CINTMULT(*aux,coeff,prime), prime);
+                    vector_add( &v2, &v1, CINTMULT(*aux,coeff,prime), prime);
                 }
             /* reduce vectors in lft in the same way */
             v2.data = lft->data; v4.data = res->data; aux = v2.data + pos;
             for ( j=0; j<lft->rows; j++, v2.data+=spr, v4.data+=uspr, aux+=spr )
                 if ( 0 != *aux ) {
-                    vector_add( &v4, &v3, CINTMULT(*aux,coeff,prime),prime);
-                    vector_add( &v2, &v1, CINTMULT(*aux,coeff,prime),prime);
+                    vector_add( &v4, &v3, CINTMULT(*aux,coeff,prime), prime);
+                    vector_add( &v2, &v1, CINTMULT(*aux,coeff,prime), prime);
                 }
         }
     }
@@ -189,4 +189,86 @@ matrix *matrix_lift( primeInfo *pi, matrix *inp, matrix *lft,
 }
 
 /*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+
+int matrix_quotient( primeInfo *pi, matrix *ker, matrix *im, 
+		     Tcl_Interp *ip, int pmsk) {
+    int i,j,cols, spr;
+    cint prime = pi->prime;
+    int failure = 1;
+    vector v1,v2;
+    cint *aux;
+    matrix m1;
+
+    PROGVARINIT ;
+ 
+    /* m1 is used to collect quotient vectors from ker */
+    m1.data = ker->data; m1.nomcols = ker->nomcols; 
+    m1.cols = ker->cols; m1.rows= 0;
+
+    cols = im->cols;
+    v1.num = v2.num = cols;
+#if 0
+    assert(im->cols == ker->cols);
+#endif
+    spr = im->nomcols; /* sints per row */
+
+    for ( v1.data=im->data, i=0; i<im->rows; i++, v1.data+=spr ) {
+        cint coeff; int pos;
+        if ((pmsk) && (0==(i&pmsk))) {
+            perc = i; perc /= im->rows;
+            perc = 1-perc; perc *= perc; perc = 1-perc;
+            PROGVARSET( perc );
+        }
+        /* find pivot for this row */
+        for ( aux=v1.data, j=cols; j; aux++, j-- )
+            if ( 0 != *aux ) break;
+        if ( 0 == j ) {  /* row is zero */ exit(1); }
+
+        else {
+            pos = aux - v1.data;
+            coeff = pi->inverse[(unsigned) *aux]; 
+	    coeff = prime-coeff; coeff %= prime;
+            /* reduce vectors in ker in the usual way */
+            v2.data = ker->data; aux = v2.data + pos;
+            for ( j=0; j<ker->rows; j++, v2.data+=spr,  aux+=spr )
+                if ( 0 != *aux ) {
+                    vector_add( &v2, &v1, CINTMULT(*aux,coeff,prime), prime );
+                }
+        }
+    }
+
+    /* now reduce ker and collect results */
+
+    PROGVARSET( -1.0 ); /* -1 to indicate begin of last phase */
+
+    for ( v1.data=ker->data, i=0; i<ker->rows; i++, v1.data+=spr ) {
+        cint coeff; int pos;
+        /* find pivot for this row */
+        for ( aux=v1.data, j=cols; j; aux++, j-- )
+            if ( 0 != *aux ) break;
+        if ( 0 == j ) {
+            /* row is zero */
+        } else {
+            pos = aux - v1.data;
+            matrix_collect( &m1, i ); /* collect this row */
+            coeff = pi->inverse[(unsigned) *aux]; 
+	    coeff = prime-coeff; coeff %= prime;
+            /* reduce other vectors in ker */
+            v2.data = v1.data + spr; aux = v2.data + pos;
+            for ( j=i+1; j<ker->rows; j++, v2.data+=spr,  aux+=spr )
+                if ( 0 != *aux ) {
+                    vector_add( &v2, &v1, CINTMULT(*aux,coeff,prime), prime );
+                }
+        }
+    }
+
+    PROGVARDONE ;
+
+    failure = 0;
+ done:; 
+
+    if (TCL_OK != matrix_resize( ker, m1.rows )) return TCL_ERROR;
+
+    return TCL_OK;
+}
 
