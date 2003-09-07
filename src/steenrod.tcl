a4129b5f7f7c9c1e59b14c0636e41e67a485cc0c
Christian Nassau <nassau@nullhomotopie.de>
2003-09-07 13:21:56 +0000

    empty first version

---
+++ src/steenrod.tcl
@@ -0,0 +1,27 @@
+# [incr Tcl] package for the Steenrod algebras 
+#
+# Copyright (C) 2003 Christian Nassau <nassau@nullhomotopie.de>
+#
+#  $Id$
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+
+package require Itcl
+catch { namespace import itcl::* }
+
+load libtprofile.so
+
+
+
+
+
+
+
+package provide steenrod 1.0
+
+
+
+
