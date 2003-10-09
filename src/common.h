8c70a73b3327e14db483e162ae4b28cecf5c9a85
Christian Nassau <nassau@nullhomotopie.de>
2003-10-09 20:31:38 +0000

    added common.h

---
+++ src/common.h
@@ -0,0 +1,23 @@
+/*
+ * Common macros, typedefs, constants, etc.
+ *
+ * Copyright (C) 2003 Christian Nassau <nassau@nullhomotopie.de>
+ *
+ *  $Id$
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
+
+#ifndef COMMON_DEF
+#define COMMON_DEF
+
+/* error codes */
+#define SUCCESS         0 
+#define FAIL            1  /* general failure */
+#define FAILMEM         2  /* out of memory */
+#define FAILIMPOSSIBLE  3  /* operation not possible */
+
+#endif
