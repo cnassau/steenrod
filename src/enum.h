62d08e32b782c1f8d61c797ffe06928b6d5cbf2b
Christian Nassau <nassau@nullhomotopie.de>
2003-12-05 21:24:07 +0000

    more configuration details

--- src/enum.h
+++ src/enum.h
@@ -25,7 +25,7 @@ typedef struct {
 
 typedef struct {
     /* description of the algebra/subalgebra pair */
-    exmo       algebra, profile;
+    exmo       algebra, profile, signature;
     primeInfo *pi;
 
     /* genList is a list of 4-tuples (gen-id, ideg, edeg, hdeg) */
@@ -43,17 +43,19 @@ typedef struct {
     int totdeg;
     int remdeg;
     int extdeg;
+    int sigideg, sigedeg; 
 
     /* tables that are used for sequence number computations */
     int effdeg[NALG];      /* this is "reddeg * profile" */
     int tablen;            /* length of the following arrays */
+    int tabmaxrideg;       /* maximal reduced internal degree */
     int *(dimtab[NALG+1]); 
     int *(seqtab[NALG+1]); 
 
     /* list of effective generators, and their sequence number offsets */
     effgen    *efflist;
     int       *seqoff;
-    int        efflen; 
+    int        efflen, maxrrideg; 
 
 } enumerator;
 
@@ -61,8 +63,27 @@ enumerator *enmCreate(void);
 enumerator *enmCopy(enumerator *src);
 void        enmDestroy(enumerator *en);
 
-/* An enumpoly is just an enumerator that is accessed through the polynomial 
- * interface */
+/* We use the lazy approach for the configuration of an enumerator, so 
+ * setting an option to a new value just invalidates all dependent fields;
+ * those are then recomputed later on demand. 
+ *
+ * Options are grouped into the following blocks:
+ *
+ *  - basics:       prime, algebra, profile 
+ *  - trideg:       ideg, edeg, hdeg, signature
+ *  - genlist:      genlist
+ */
+
+int enmSetBasics(enumerator *en, primeInfo *pi, exmo *algebra, exmo *profile);
+int enmSetSignature(enumerator *en, exmo *sig);
+int enmSetTridegree(enumerator *en, int ideg, int edeg, int hdeg);
+int enmSetGenlist(enumerator *en, int *gl, int num);
+
+/* enumeration uses these procedures. the enumerated exmo is "varex". */
+int firstRedmon(enumerator *en);
+int nextRedmon(enumerator *en);
+
+/* An enumpoly allows to access an enumerator as a polynomial */
 
 #ifndef ENUMC
 extern polyType enumPolyType;
