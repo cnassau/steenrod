5b046d6ccad73ec48068e8a74be37e5cdbc84a1f
Christian Nassau <nassau@nullhomotopie.de>
2003-12-13 11:05:27 +0000

    added install target & utility

---
+++ src/instlib.tcl
@@ -0,0 +1,33 @@
+#!/usr/bin/tclsh
+#
+# Move (library) file to platform specific library path  
+#
+# Copyright (C) 2003 Christian Nassau <nassau@nullhomotopie.de>
+#
+#  $Id$
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+
+set targdir [file join .. lib $tcl_platform(os)]
+
+if {[llength $argv] != 1} {
+    puts "use \"$argv0 <file>\" to move file into $targdir"
+    exit 1
+}
+
+set fname [lindex $argv 0]
+
+if {![file readable $fname]} {
+    puts "Can't read file $fname"
+    exit 1
+}
+
+if {![file isdirectory $targdir]} {
+    file mkdir $targdir
+}
+
+file copy $fname $targdir
+file delete $fname 
