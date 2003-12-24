77370f4635e767cb66ecc37312c6a82569c9bacc
Christian Nassau <nassau@nullhomotopie.de>
2003-12-24 13:37:44 +0000

    added header

--- doc/makedocs.tcl
+++ doc/makedocs.tcl
@@ -10,6 +10,19 @@ cd html
 ::doctools::new 2tmml -format tmml
 cd ..
 
+#!/usr/bin/tclsh
+#
+# Create html, tmml, and manpage from *.msrc
+#
+# Copyright (C) 2003 Christian Nassau <nassau@nullhomotopie.de>
+#
+#  $Id$
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+
 set flst [glob -nocomplain *.msrc]
 
 puts "Converting documentation, file pattern = *.msrc"
