b1f7a2388e1745bf8e924105998aed5d68c260d1
Christian Nassau <nassau@nullhomotopie.de>
2005-12-10 12:16:21 +0000

    added custom Tcl shell (eg. for use with gprof)

---
+++ src/steenrodsh.c
@@ -0,0 +1,39 @@
+/*
+ * Custom Tcl shell with built-in Steenrod library
+ *
+ * Copyright (C) 2005 Christian Nassau <nassau@nullhomotopie.de>
+ *
+ *  $Id$
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
+
+#include <tcl.h>
+
+extern int Steenrod_Init(Tcl_Interp *ip);
+
+int Tcl_SteenAppInit(Tcl_Interp *interp) 
+{
+    if (Tcl_Init(interp) == TCL_ERROR) {
+	return TCL_ERROR;
+    }
+
+    if (Steenrod_Init(interp) ==  TCL_ERROR) {
+        return TCL_ERROR;
+    }
+
+    Tcl_Eval(interp, "puts [format {"
+             "Tcl shell with Steenrod algebra support (version %s, "
+             "built " __DATE__ " " __TIME__ ")"
+             "} [package require Steenrod]]");
+    
+    return TCL_OK;
+}
+
+int main(int argc, char **argv) {
+    Tcl_Main(argc, argv, Tcl_SteenAppInit);
+    return 0;
+}
